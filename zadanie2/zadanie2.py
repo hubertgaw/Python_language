72ff76cc3b2c86709fdf0a944dd7987c0db5b44e
Hubert Gawlowski <hubertgaw@gmail.com>
2020-11-08 22:56:18 +0100

    Add metdos sheeps_init and sheeps_move

--- zadanie2/zadanie2.py
+++ zadanie2/zadanie2.py
@@ -26,15 +26,40 @@ class Wolf:
     y = 0.0
 
 
-def main():
+# metoda inicjalizujace owce ze wstepnymi wartosciami:
+def sheeps_init():
     sheeps_list = []
     for i in range(sheeps_number):
         sheeps_list.append(Sheep(random.uniform(-init_pos_limit, init_pos_limit),
                                  random.uniform(-init_pos_limit, init_pos_limit)))
-    licznik = 1
-    for sheep in sheeps_list:
-        print("Owca nr.", licznik, "x:", sheep.x, "y:", sheep.y, "direction:", sheep.direction, "status:", sheep.status)
-        licznik = licznik + 1
+
+    for index, sheep in enumerate(sheeps_list):  # dzięki enumerate dostajemy indeks owcy w tablicy
+        print("Owca nr.", index, "x:", sheep.x, "y:", sheep.y, "direction:", sheep.direction, "status:", sheep.status)
+    return sheeps_list
+
+
+# metoda odpowiadająca za ruch owcy:
+def sheeps_move(sheeps_list):
+    directions = ["N", "S", "E", "W"]
+    for i in range(rounds_number):
+        print("--------------------RUNDA:", i, "--------------------")
+        for sheep in sheeps_list:
+            sheep.direction = random.choice(directions)
+            if sheep.direction == 'N':
+                sheep.y = sheep.y + sheep_move_dist
+            elif sheep.direction == 'S':
+                sheep.y = sheep.y - sheep_move_dist
+            elif sheep.direction == 'E':
+                sheep.x = sheep.x + sheep_move_dist
+            elif sheep.direction == 'W':
+                sheep.x = sheep.x - sheep_move_dist
+        for index, sheep in enumerate(sheeps_list):  # dzięki enumerate dostajemy indeks owcy w tablicy
+            print("Owca nr.", index, "x:", sheep.x, "y:", sheep.y, "direction:", sheep.direction, "status:",
+                  sheep.status)
+
+
+def main():
+    sheeps_move(sheeps_init())
 
 
 if __name__ == "__main__":
