44cb6fef54793d28ff4a2e46bc5812411d9762fe
Hubert Gawlowski <hubertgaw@gmail.com>
2020-11-08 22:16:19 +0100

    Change class Sheep and add sheeps to list, initialize thair start points

--- zadanie2/zadanie2.py
+++ zadanie2/zadanie2.py
@@ -1,13 +1,20 @@
+import random
+
+rounds_number = 50
+sheeps_number = 15
+init_pos_limit = 10.0
+sheep_move_dist = 0.5
+
+
 class Sheep:
     # atrybuty klasowe, czyli wspoldzielona przez wszystkie obiekty klasy:
-    init_pos_limit = 10.0
-    sheep_move_dist = 0.5
 
     # atrybuty instancyjne, czyli związane z okreslonym obiektem:
-    def __init__(self, x, y, direction):
-        self.x = x
-        self.y = y
+    def __init__(self, x, y, direction='N'):
+        self.x = random.uniform(-init_pos_limit, init_pos_limit)
+        self.y = random.uniform(-init_pos_limit, init_pos_limit)
         self.direction = direction
+        self.status = 'alive'
 
 
 # Byc moze w owcy wystarczą atrybuty x i y, jeszcze to przemysle
@@ -17,3 +24,18 @@ class Wolf:
     wolf_move_dist = 1.0
     x = 0.0
     y = 0.0
+
+
+def main():
+    sheeps_list = []
+    for i in range(sheeps_number):
+        sheeps_list.append(Sheep(random.uniform(-init_pos_limit, init_pos_limit),
+                                 random.uniform(-init_pos_limit, init_pos_limit)))
+    licznik = 1
+    for sheep in sheeps_list:
+        print("Owca nr.", licznik, "x:", sheep.x, "y:", sheep.y, "direction:", sheep.direction, "status:", sheep.status)
+        licznik = licznik + 1
+
+
+if __name__ == "__main__":
+    main()
